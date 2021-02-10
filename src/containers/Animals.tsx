341a88f72279829f383db4bce0d14c81d301950d
ACAPP-dev <acapp909@gmail.com>
2021-02-09 19:19:02 -0800

    adding form to narrow view animals

--- src/containers/Animals.tsx
+++ src/containers/Animals.tsx
@@ -4,6 +4,12 @@ import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { AnimalCard } from "../components/AnimalCard";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
 // import { fchmod } from "fs";
 
 const animal = [
@@ -27,6 +33,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     grid: {
       justifyContent: "center",
+      marginTop: 2,
     },
     paper: {
       padding: theme.spacing(2),
@@ -36,12 +43,31 @@ const useStyles = makeStyles((theme: Theme) =>
     heading: {
       textAlign: "center",
     },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      textAlign: "center",
+      marginLeft: 50,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
   })
 );
 export const Animals = () => {
   const [animals, setAnimals] = useState<typeof animal>(Object);
   const [term, setTerm] = useState<string>("");
+  const [search, setSearch] = useState<string>("");
+  const [selection, setSelection] = useState<string>("");
+
   const classes = useStyles();
+
+  const handleSearch = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSearch(event.target.value as string);
+    console.log(search);
+  };
+
+  const handleSelection = (e: any) => {};
 
   useEffect(() => {
     axios
@@ -65,6 +91,18 @@ export const Animals = () => {
   return (
     <div>
       <h1 className={classes.heading}>Available for Adoption</h1>
+      <div className="form">
+        <form>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Narrow Search</InputLabel>
+            <Select onChange={handleSearch} value={search}>
+              <MenuItem value={1}>Species</MenuItem>
+              <MenuItem value={2}>Breed</MenuItem>
+              <MenuItem value={3}>Gender</MenuItem>
+            </Select>
+          </FormControl>
+        </form>
+      </div>
       <div className={classes.root}>
         <Grid className={classes.grid} container spacing={3}>
           {getAnimalCards()}
